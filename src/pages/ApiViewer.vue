8cbfcf103c4e0141939d9168779feef4a50c8038
root <root@Mobile-Storm.>
2025-04-04 02:28:20 -0400

    Adjust apiviewer for versioning

--- src/pages/ApiViewer.vue
+++ src/pages/ApiViewer.vue
@@ -3,26 +3,61 @@
     class="bg-secondary text-white shadow-2"
     inset
   >
-
-    <div class="q-pa-md q-gutter-sm">
-
-      Current Data Api Url:
-      <div class="text-h6">
-        {{ currentUrl }}
-      </div>
-    </div>
-    <q-space />
     <q-select
       filled
+      stack-label
       dense
+      options-dense
+      emit-value
+      map-options
       v-model="currentUrl"
-      :options="urls"
+      :options="getVersionList"
       label="Choose a api to view"
+      style="min-width: 250px; max-width: 300px"
     >
-      <template v-slot:prepend>
+      <!-- <template v-slot:prepend>
         <q-icon name="travel_explore" />
+      </template> -->
+      <template v-slot:option=" scope ">
+        <q-item v-bind="scope.itemProps">
+          <q-item-section avatar>
+            <q-icon name="link" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ scope.opt.label }}</q-item-label>
+            <q-item-label caption>{{ scope.opt.value }}</q-item-label>
+          </q-item-section>
+        </q-item>
       </template>
     </q-select>
+    <q-list>
+      <q-item>
+        <q-item-section>
+          <q-item-label>Current Data Api:</q-item-label>
+          <q-item-label caption>
+            <a
+              class="text-overline"
+              :href="currentUrl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {{ currentUrl }}</a>
+          </q-item-label>
+        </q-item-section>
+
+        <q-item-section
+          side
+          top
+        >
+          <div class="text-primary text-caption q-pl-xl">
+            Latest: <q-badge color="accent">v{{ apiVersion }}.0.0+</q-badge>
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+    <q-space />
+
   </q-toolbar>
   <div class="flex items-center">
     <div v-if=" loading ">Loading...</div>
@@ -35,25 +70,38 @@
     </pre>
   </div>
   <q-ajax-bar
-      ref="bar"
-      position="bottom"
-      color="secondary"
-      size="10px"
-    />
+    ref="bar"
+    position="bottom"
+    color="secondary"
+    size="10px"
+  />
 </template>
 <script setup>
 import { useThoughtStore } from 'src/stores/thoughts.js'
 import { storeToRefs } from 'pinia'
-import { onMounted, watch, ref } from 'vue'
+import { onMounted, watch, ref, computed } from 'vue'
 const bar = ref(null)
 const thoughtStore = useThoughtStore()
-const { error, loading, currentUrl, thoughts, urls } = storeToRefs( thoughtStore )
+const { error, loading, currentUrl, thoughts, urls, apiVersion } = storeToRefs( thoughtStore )
+
+
 const refresh = ( url ) => {
   const barRef = bar.value
   barRef.start()
   thoughtStore.getThoughts( url )
   barRef.stop()
 }
+
+const getVersionList = computed( () => {
+  const versions = urls.value.map( ( url, idx ) => {
+    let obj = {}
+    obj['label'] = `v${idx + 1}`
+    obj['value'] = url
+    return obj
+  } )
+  console.dir( versions )
+  return versions
+} )
 
 watch(
   () => thoughtStore.currentUrl,
--- src/stores/thoughts.js
+++ src/stores/thoughts.js
@@ -1,31 +1,39 @@
 import { defineStore, acceptHMRUpdate } from 'pinia'
 import jsonPath from 'jsonpath'
 
-export const useThoughtStore = defineStore('thoughtStore', {
-  state: () => ({
+export const useThoughtStore = defineStore( 'thoughtStore', {
+  state: () => ( {
     loading: false,
     error: null,
     thoughts: {},
     currentUrl: '',
     nextId: 0,
-    urls: ['http://localhost:3000/Purchases', 'http://localhost:3001/0'],
-  }),
+    urls: ['http://localhost:3002/Purchases', 'http://localhost:3000/Purchases', 'http://localhost:3001/0'],
+  } ),
   getters: {
+    apiVersion () {
+      return this.urls.length 
+    },
     lastId () {
       try {
         // get all ids
         var ids = jsonPath.query( this.thoughts, "$..id" );
-
-        this.nextId = Math.max(...ids) + 1
+        // get the max id in the object and increment
+        this.nextId = Math.max( ...ids ) + 1
       } catch ( err ) {
         console.log( err )
         this.nextId = err
       }
+      return this.nextId
     }
   },
   actions: {
-    // get data from api (default to first item in urls array)
+    queryData ( jsonPathQuery ) {
+      //query data using jsonpath
+      return jsonPath.query( this.data, jsonPathQuery );
+    },
     async getThoughts ( url ) {
+      // get data from api (default to first item in urls array)
       if ( !url ) url = this.urls[0]
       this.loading = true
       this.currentUrl = url
